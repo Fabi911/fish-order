7ec38c8959c5cc1f943dd0e578bd44814e2dd8c5
Fabian Döz <fabian@doez.info>
2024-11-01 21:48:15 +0100

    add mockuser

--- backend/src/test/java/de/fvleineck/backend/order/controller/OrderControllerTest.java
+++ backend/src/test/java/de/fvleineck/backend/order/controller/OrderControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -22,6 +23,7 @@ class OrderControllerTest {
 
 
 	@Test
+	@WithMockUser(roles="ADMIN")
 	void createOrder () throws Exception {
 		mvc.perform(MockMvcRequestBuilders.post("/api/orders")
 				.contentType(MediaType.APPLICATION_JSON)
@@ -55,6 +57,7 @@ class OrderControllerTest {
 	}
 
 	@Test
+	@WithMockUser(roles="ADMIN")
 	void getAllOrders () throws Exception {
 		mvc.perform(MockMvcRequestBuilders.get("/api/orders"))
 				.andExpect(MockMvcResultMatchers.status().isOk())
@@ -62,6 +65,7 @@ class OrderControllerTest {
 	}
 
 	@Test
+	@WithMockUser(roles="ADMIN")
 	void deleteOrder() throws Exception {
 		// Create a new order before deleting
 		mvc.perform(MockMvcRequestBuilders.post("/api/orders")
