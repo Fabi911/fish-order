1890051a4065f91c674d51122120b99bd3f9c43d
Fabian Döz <fabian@doez.info>
2025-01-26 22:08:04 +0100

    Update order details, prices, and styles for 2025 event
    Adjusted all references to the new event date (18.04.2025) and updated prices for smoked and fresh trout. Improved UI styles with new colors and shadows, enhancing the overall design. Modified routing and elements to align with updated functionality and event needs.

--- frontend/src/pages/OrderPage.tsx
+++ frontend/src/pages/OrderPage.tsx
@@ -7,10 +7,10 @@ export default function OrderPage() {
 		<>
 			<h1 className="orderPage">Bestellung Forellenverkauf</h1>
 			<article className="article">
-				<h2>für den 22.12.2024</h2>
+				<h2>für Karfreitag, den 18.04.2025</h2>
 				<h3>Preise:</h3>
-				<p className="price">geräucherte Forelle: 7,50€</p>
-				<p>eingelegte Forelle: 6,00€</p>
+				<p className="price">geräucherte Forelle: 8,50€</p>
+				<p>eingelegte Forelle: 7,00€</p>
 			</article>
 			<OrderForm />
 			<br/>
